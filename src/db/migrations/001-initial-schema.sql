fae3525834b00daf59c8e2ce3c0194177787386b
Júlia Küchle <juliagkuchle@gmail.com>
2023-11-07 14:52:56 -0300

    fix database column name

--- src/db/migrations/001-initial-schema.sql
+++ src/db/migrations/001-initial-schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS events (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(50),
     description VARCHAR(50),
-    data VARCHAR(50),
+    date VARCHAR(50),
     duration VARCHAR(50),
     seats INTEGER
 );
